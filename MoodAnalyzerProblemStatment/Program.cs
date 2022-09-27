963aa205ce9e831d24b0e7c63b1dc2e1f3f15035
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-27 21:29:49 +0530

    [Anshuman] Added: Welcome Message

---
+++ MoodAnalyzerProblemStatment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblemStatment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problem Statment");
+        }
+    }
+}
